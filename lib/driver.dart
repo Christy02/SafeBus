367e7157851b83098199f17357e046d8af1f5981
Christy02 <christyvarghese124@gmail.com>
2023-04-21 00:23:49 +0530

    bg corrected

--- lib/driver.dart
+++ lib/driver.dart
@@ -69,143 +69,141 @@ class _DriverPageState extends State<DriverPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Driver'),
-        backgroundColor: Color.fromARGB(255, 117, 154, 255),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const BusPage()),
-                );
-              },
-              icon: const Icon(
-                Icons.directions_bus_filled_sharp,
-                size: 30,
-              ))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _handleTap,
-        backgroundColor:
-            _tapCount > 2 ? Color.fromARGB(255, 221, 10, 10) : Colors.green,
-        child: const Icon(Icons.bus_alert_rounded, size: 30),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 100,
-              left: 25,
-              right: 25,
-            ),
-            child: Row(
-              children: <Widget>[
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white70,
-                        elevation: 3,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 50,
-                          horizontal: 40,
-                        )),
-                    onPressed: () {
-                      Route route =
-                          MaterialPageRoute(builder: (context) => NoticePage());
-                      Navigator.push(context, route);
-                    },
-                    child: const Text(
-                      'Notices',
-                      style: TextStyle(
-                        letterSpacing: 1.5,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'OpenSans',
-                        color: Color.fromARGB(255, 117, 154, 255),
-                      ),
-                    )),
-                const SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white70,
-                        elevation: 3,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 50,
-                          horizontal: 50,
-                        )),
-                    onPressed: () {
-                      Route route =
-                          MaterialPageRoute(builder: (context) => ChatScreen());
-                      Navigator.push(context, route);
-                    },
-                    child: const Text(
-                      'Chat',
-                      style: TextStyle(
-                        letterSpacing: 1.5,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'OpenSans',
-                        color: Color.fromARGB(255, 117, 154, 255),
-                      ),
-                    )),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 70,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 25,
-              right: 25,
-            ),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.white70,
-                    elevation: 3,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      vertical: 30,
-                      horizontal: 75,
-                    )),
-                onPressed: () {
-                  Route route =
-                      MaterialPageRoute(builder: (context) => AttendancePage());
-                  Navigator.push(context, route);
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Driver'),
+          backgroundColor: Color.fromARGB(255, 117, 154, 255),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const BusPage()),
+                  );
                 },
-                child: const Text(
-                  'Mark Attendance',
-                  style: TextStyle(
-                    letterSpacing: 1.5,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'OpenSans',
-                    color: Color.fromARGB(255, 117, 154, 255),
-                  ),
-                )),
-          ),
-          SizedBox(
-            height: 203,
-          ),
+                icon: const Icon(
+                  Icons.directions_bus_filled_sharp,
+                  size: 30,
+                ))
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _handleTap,
+          backgroundColor:
+              _tapCount > 2 ? Color.fromARGB(255, 221, 10, 10) : Colors.green,
+          child: const Icon(Icons.bus_alert_rounded, size: 30),
+        ),
+        body: Stack(children: <Widget>[
           Positioned(
             bottom: 0,
             right: 0,
             child: Image.asset("assets/bottom2.png", width: size.width),
           ),
-        ],
-      ),
-    );
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 100,
+                  left: 25,
+                  right: 25,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.white70,
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 50,
+                              horizontal: 40,
+                            )),
+                        onPressed: () {
+                          Route route = MaterialPageRoute(
+                              builder: (context) => NoticePage());
+                          Navigator.push(context, route);
+                        },
+                        child: const Text(
+                          'Notices',
+                          style: TextStyle(
+                            letterSpacing: 1.5,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'OpenSans',
+                            color: Color.fromARGB(255, 117, 154, 255),
+                          ),
+                        )),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.white70,
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 50,
+                              horizontal: 50,
+                            )),
+                        onPressed: () {
+                          Route route = MaterialPageRoute(
+                              builder: (context) => ChatScreen());
+                          Navigator.push(context, route);
+                        },
+                        child: const Text(
+                          'Chat',
+                          style: TextStyle(
+                            letterSpacing: 1.5,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'OpenSans',
+                            color: Color.fromARGB(255, 117, 154, 255),
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25,
+                  right: 25,
+                ),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white70,
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 30,
+                          horizontal: 75,
+                        )),
+                    onPressed: () {
+                      Route route = MaterialPageRoute(
+                          builder: (context) => AttendancePage());
+                      Navigator.push(context, route);
+                    },
+                    child: const Text(
+                      'Mark Attendance',
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'OpenSans',
+                        color: Color.fromARGB(255, 117, 154, 255),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+        ]));
   }
 }
